36d3732521e1a33f795405f85d1ee328caf7bd30
laeo <8142123+laeo@users.noreply.github.com>
2025-06-27 18:04:35 +0800

    fix: verify the tailwindcss in ui package works successful

---
+++ apps/web/app/shared-component/page.tsx
@@ -0,0 +1,9 @@
+import { VideoCard } from 'ui';
+
+export default function SharedComponentPage() {
+    return (
+        <main className=' flex flex-col items-center justify-center min-h-screen p-4'>
+            <VideoCard src={''}></VideoCard>
+        </main>
+    );
+}
--- packages/ui/src/components/VideoCard/index.tsx
+++ packages/ui/src/components/VideoCard/index.tsx
@@ -1,9 +1,14 @@
+'use client';
+
 type VideoCardProps = {
     src: string;
+    autoPlay?: boolean;
 }
 
 export default function VideoCard(props: VideoCardProps) {
+    const { src, autoPlay = false } = props;
+
     return (
-        <div></div>
-    )
+        <video src={src} className="aspect-[9/16]" autoPlay={autoPlay}></video>
+    );
 }
